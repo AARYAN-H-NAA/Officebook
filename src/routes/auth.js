1b5a32b4884f8f83339f5c2027682f38308f742f
Aaryan <87814002+AARYAN-H-NAA@users.noreply.github.com>
2024-02-13 17:57:39 +0530

    updated1

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,32 +1,24 @@
 // routes/auth.js
 
 const express = require('express');
-
 const router = express.Router();
-
-const jwt = require('jsonwebtoken');
-
 const User = require('../models/user');
+const { register, login, logout } = require('../controllers/authController');
 
+router
+    // Register
+    .get('/register', (req, res, next) => {
+        res.render('authRegister');
+    })
+    .post('/register', register)
 
+    // Login
+    .get('/login', (req, res, next) => {
+        res.render('authLogin');
+    })
+    .post('/login', login)
 
-const {register , login , logout} = require('../controllers/authController')
-
-
-router.get('/register', (req, res, next) => {
-    res.render('authRegister');
-});
-
-router.get('/login', (req, res, next) => {
-    res.render('authLogin');
-});
-
-// Registration
-router.post('/register', register);
-
-// Login
-router.post('/login', login);
-
-router.post('/logout', logout)
+    //Logout
+    .post('/logout', logout);
 
 module.exports = router;
