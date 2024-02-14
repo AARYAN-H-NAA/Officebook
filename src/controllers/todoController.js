59cfe67fa3ed02238ecb5c61beddd009320a7173
Aaryan <87814002+AARYAN-H-NAA@users.noreply.github.com>
2024-02-14 11:33:04 +0530

    Update1

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,39 +1,57 @@
-const mongoose = require('mongoose');
 const Task = require('../models/todo');
 
-exports.handleTaskRequest = async (req, res) => {
-    const userId = req.userId;
-
-    try {
-        if (req.method === 'GET' && req.path === '/tasks/todo') {
-            // Get all tasks
+const todoController = {
+    async getTodo(req, res, next) {
+        try {
+            const userId = req.userId;
             const tasks = await Task.find({ userId }).exec();
-            const tasksArray = Array.isArray(tasks) ? tasks : [tasks];
-            res.render('todo', { task: tasksArray });
-        } else if (req.method === 'POST' && req.path === '/tasks/todo') {
-            // Add new task
-            const newTask = new Task({ task: req.body.tasks, userId });
-            await newTask.save();
-            res.redirect(req.originalUrl);
-        } else if (req.method === 'DELETE' && req.path === '/tasks/todo/delete-task') {
-            // Delete task
-            const taskId = req.body.taskId;
-            await Task.findByIdAndDelete(taskId);
-            res.redirect('/tasks/todo');
-        } else if (req.method === 'GET' && req.path.startsWith('/tasks/todo/edit-task/')) {
-            // Get edit task page
-            const taskId = req.path.split('/').pop();
-            const task = await Task.findById(taskId).exec();
-            res.render('task', { task });
-        } else if (req.method === 'POST' && req.path.startsWith('/tasks/todo/edit-task/')) {
-            // Edit task
-            const taskId = req.path.split('/').pop();
-            const updatedTask = req.body.updatedTask;
-            await Task.findOneAndUpdate({ _id: taskId }, { task: updatedTask });
-            res.redirect('/tasks/todo');
+            res.render('todo', { task: Array.isArray(tasks) ? tasks : [tasks] });
+        } catch (error) {
+            handleError(res, error);
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+    },
+
+    async postTodo(req, res) {
+        try {
+            const userId = req.userId;
+            await new Task({ task: req.body.tasks, userId }).save();
+            res.redirect(req.originalUrl);
+        } catch (error) {
+            handleError(res, error);
+        }
+    },
+
+    async deleteTask(req, res) {
+        try {
+            await Task.findByIdAndDelete(req.body.taskId);
+        } catch (error) {
+            handleError(res, error);
+        }
+        res.redirect('/tasks/todo');
+    },
+
+    async getEditTasks(req, res) {
+        try {
+            const task = await Task.findById(req.params.id).exec();
+            res.render('task', { task });
+        } catch (error) {
+            handleError(res, error);
+        }
+    },
+
+    async postEditTask(req, res) {
+        try {
+            await Task.findOneAndUpdate({ _id: req.params.id }, { task: req.body.updatedTask });
+        } catch (error) {
+            handleError(res, error);
+        }
+        res.redirect('/tasks/todo');
     }
 };
+
+function handleError(res, error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+}
+
+module.exports = todoController;
