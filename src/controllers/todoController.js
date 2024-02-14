df00be7aa636c02050154fb97e0f35783f417be0
Aaryan <87814002+AARYAN-H-NAA@users.noreply.github.com>
2024-02-14 11:31:20 +0530

    Update1

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,69 +1,39 @@
 const mongoose = require('mongoose');
+const Task = require('../models/todo');
 
-const Task = require('../models/todo')
+exports.handleTaskRequest = async (req, res) => {
+    const userId = req.userId;
 
-exports.getTodo = async (req, res, next) => {
-    const userId = req.userId
     try {
-      const tasks = await Task.find({userId}).exec();
-      const tasksArray = Array.isArray(tasks) ? tasks : [tasks]; // Ensure tasks is an array
-      res.render('todo', { task: tasksArray });
+        if (req.method === 'GET' && req.path === '/tasks/todo') {
+            // Get all tasks
+            const tasks = await Task.find({ userId }).exec();
+            const tasksArray = Array.isArray(tasks) ? tasks : [tasks];
+            res.render('todo', { task: tasksArray });
+        } else if (req.method === 'POST' && req.path === '/tasks/todo') {
+            // Add new task
+            const newTask = new Task({ task: req.body.tasks, userId });
+            await newTask.save();
+            res.redirect(req.originalUrl);
+        } else if (req.method === 'DELETE' && req.path === '/tasks/todo/delete-task') {
+            // Delete task
+            const taskId = req.body.taskId;
+            await Task.findByIdAndDelete(taskId);
+            res.redirect('/tasks/todo');
+        } else if (req.method === 'GET' && req.path.startsWith('/tasks/todo/edit-task/')) {
+            // Get edit task page
+            const taskId = req.path.split('/').pop();
+            const task = await Task.findById(taskId).exec();
+            res.render('task', { task });
+        } else if (req.method === 'POST' && req.path.startsWith('/tasks/todo/edit-task/')) {
+            // Edit task
+            const taskId = req.path.split('/').pop();
+            const updatedTask = req.body.updatedTask;
+            await Task.findOneAndUpdate({ _id: taskId }, { task: updatedTask });
+            res.redirect('/tasks/todo');
+        }
     } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-  }
-
-  exports.postTodo = async (req, res) => {
-    const userId = req.userId
-    try {
-      const newTask = new Task({ task: req.body.tasks , userId: userId});
-      await newTask.save();
-      res.redirect(req.originalUrl);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-
-  }
-
-  exports.deleteTask = async (req, res) => {
-    try {
-      const taskId = req.body.taskId;
-  
-      // Use Mongoose to find and delete the task by its ID
-      await Task.findByIdAndDelete(taskId);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-    res.redirect('/tasks/todo');
-  }
-
-  exports.getEditTasks = async (req, res) => {
-    try {
-      const taskId = req.params.id;
-      const task = await Task.findById(taskId).exec();
-      res.render('task', { task });
-      
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  }
-
-
-  exports.postEditTask = async (req, res) => {
-    try {
-      const taskId = req.params.id;
-      const updatedTask = req.body.updatedTask;
-  
-  
-      // Use Mongoose to find and delete the task by its ID
-      await Task.findOneAndUpdate({_id : taskId }, {task:updatedTask});
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-    res.redirect('/tasks/todo');
-  }
+};
