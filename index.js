bfc7f8289fbf621a90caef9467179561cbd1b570
Aaryan <87814002+AARYAN-H-NAA@users.noreply.github.com>
2024-02-20 15:29:48 +0530

    update2

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ const bodyParser = require('body-parser');
 const taskRoutes = require('./src/routes/tasks');
 const dbController = require('./src/database/dbController');
 const authRoutes = require('./src/routes/auth');
-const adminRoutes = require("./src/routes/admin")
+const adminRoutes = require("./src/routes/admin");
 const app = express();
 dbController();
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 const PORT = process.env.PORT || 5000;
 
-app.use("/auth", authRoutes);
-app.use('/tasks',taskRoutes);
-app.use('/admin',adminRoutes);
-app.get('/', (req, res) => {
+app
+  .use("/auth", authRoutes)
+  .use('/tasks', taskRoutes)
+  .use('/admin', adminRoutes)
+  .get('/', (req, res) => {
     res.send('<button type="submit"><a href="/auth/register">Register</a></button><button type="submit"><a href="/auth/login">Login</a></button>');
-});
-
-app.listen(PORT, () => {
-    console.log(`Hi server is running at Port: ${PORT}`);
+  })
+  .listen(PORT, () => {
+  console.log(`Hi server is running at Port: ${PORT}`);
 });
