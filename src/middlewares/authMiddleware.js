a6dda14dcf03b5382a222cf063a2b716b9d22acc
Aaryan <87814002+AARYAN-H-NAA@users.noreply.github.com>
2024-02-23 12:23:10 +0530

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,55 +1,30 @@
 // middlewares/authMiddleware.js
 
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const cookieParser =require('cookie-parser');
 const { request } = require('express');
+
 
 const authMiddleware = (req, res, next) => {
     cookieParser()(req, res, () => {});
-
     // Extract token from cookies
     const token = req.cookies.token;
     if (!token) {
         return res.status(401).json({ message: 'Access denied, no token provided' });
     }
-
     try {
-        const decoded = jwt.verify(token, 'MY_SECRET_KEY');
+        const decoded = jwt.verify(token, process.env.jwtKey);
+        req.userId = decoded.id;
+        req.userRole = decoded.role;
+        next();
 
-        if (decoded.role === "user") {
-            req.userId = decoded.id;
-            next();
-        }
     } catch (error) {
         console.error(error);
         res.status(401).json({ message: 'Invalid token' });
     }
 };
 
-const adminauthMiddleware = (req, res, next) => {
-    cookieParser()(req, res, () => {});
 
-    // Extract token from cookies
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({ message: 'Access denied, no token provided' });
-    }
 
-    try {
-        const decoded = jwt.verify(token, 'MY_SECRET_KEY');
 
-        // If userRole is admin
-        if (decoded.role === "admin") {
-            req.userId = decoded.id;
-            next();
-        } else {
-            res.redirect("/tasks/todo");
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(401).json({ message: 'Invalid token' });
-    }
-};
-
-module.exports = { adminauthMiddleware, authMiddleware };
-
+module.exports ={authMiddleware}
